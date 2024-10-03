88c6ad932d80f747c7a02b645126a2d3b7ce549a
kanats <kanats007m@gmail.com>
2024-10-03 23:05:09 +0900

    不要なファイル削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,7 @@
 
 'use client';
 import { LoginButton } from "@/components/LoginButton";
-import { useLogin } from "@/hooks/useLogin";
 
 export default function Login() {
-  // useLogin();
   return <LoginButton/>;
 }
